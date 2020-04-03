90bb54aa3b4e7fe03486b02e9ed221bfc5ea82f3
18097716 <18097716@cnsuning.com>
2020-04-03 19:42:23 +0800

    add loading

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xv
+
+# scp -r * root@106.54.117.79:/usr/share/nginx/html
+scp -r * root@47.95.214.138:/home/dist/static/car
--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var renderer;
 var light;
 var loader;
 var orbit;
+var composer;
 
 // initThree initThree
 var initThree = function(){
@@ -17,62 +18,72 @@ var initThree = function(){
     orbit = new THREE.OrbitControls(camera, renderer.domElement)
 
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
     
 
     document.body.appendChild(renderer.domElement);
+
+    Pace.on('done', function(){
+        document.getElementById('nav').style.display = 'inline-block'
+        console.log(123213)
+    })
 };
+
+var tuggleRotation = function() {
+    orbit.autoRotate = !orbit.autoRotate
+}
 
 window.onload = function(){
     initThree();
     
-    camera.position.set(7, 7, 7);
+    camera.position.set(10, 8, 11);
     camera.lookAt(0,0,0);
 
-    // 地面
-    let geometryPlane = new THREE.PlaneGeometry(1000,1000);
-    let materialPlane = new THREE.MeshBasicMaterial({color: 0xA9A9A9});
-    let rect = new THREE.Mesh(geometryPlane,materialPlane);
-    rect.rotation.x = -1.58;
-    rect.position.set(0, 0, 0);
-    scene.add(rect);
-
     // 点光源
-    pLight = new THREE.PointLight( 0xFFFFFF, 15, 300 );
-    pLight.position.set( 200, 130, 40 );
+    pLight = new THREE.PointLight( 0xFFFFFF, 1, 50 );
+    pLight.position.set( -15, 9, 6 );
     scene.add( pLight );
 
 
     // 点光源2
-    pLight2 = new THREE.PointLight( 0xFFFFFF, 35, 300 );
-    pLight2.position.set( -140, 130, 220 );
+    pLight2 = new THREE.PointLight( 0xFFFFE0, 2, 50 );
+    pLight2.position.set( -15, 9, -7 );
     scene.add( pLight2 );
 
-    // 点光源3
-    pLight3 = new THREE.PointLight( 0xFFFFFF, 25, 300 );
-    pLight3.position.set( -180, 200, -250 );
-    scene.add( pLight3 );
- 
-
-    // // 环境光
-    // hLight = new THREE.AmbientLight(0xFFFFFF, 10);
-    // scene.add(hLight);
-
     // 平行光
-    // hLight = new THREE.DirectionalLight(0xFFFFFF, 3);
-    // hLight.position.set(10, 200, 10)
-    // scene.add(hLight);
-
-
-    // 渲染
-    // renderer.render(scene, camera);
+    dLight = new THREE.DirectionalLight(0xFFFFE0, 1.2);
+    dLight.position.set( 100, 100, 300 );
+    scene.add(dLight);
 
     // 控制
     orbit.addEventListener('change', function(){
         
     })
+    // 自动旋转和速度
     orbit.autoRotate = true
-    orbit.autoRotateSpeed = 1
-    orbit.dampingFactor = 0.01; 
+    orbit.autoRotateSpeed = 0.3
+    orbit.enableDamping = true
+    orbit.dampingFactor = 0.25;
+    orbit.zoomSpeed = 0.5;
+    // 视角的最大仰角和俯角
+    orbit.minPolarAngle = Math.PI / 5;
+    orbit.maxPolarAngle = Math.PI / 2.1;
+    // 距离中心点最小距离和最大距离
+    orbit.minDistance = 10;
+    orbit.maxDistance = 100
+
+    // post-processing 后处理
+    composer = new POSTPROCESSING.EffectComposer(renderer);
+    composer.addPass(new POSTPROCESSING.RenderPass(scene, camera))
+    const effectPass = new POSTPROCESSING.EffectPass(camera, new POSTPROCESSING.BloomEffect()) // 辉光
+    // const effectPass = new POSTPROCESSING.EffectPass(camera, new POSTPROCESSING.DepthOfFieldEffect()) // 景深
+    effectPass.renderToScreen = true
+    composer.addPass(effectPass)
+
+    const manager = new THREE.LoadingManager()
+    manager.onLoad = function ( ) {
+        console.log( "Loading complete!")
+    }
 
     loader.load('./models/garage/scene.gltf', function(gltf) {
         scene.add(gltf.scene);
@@ -83,12 +94,12 @@ window.onload = function(){
         gltf.scene.position.y = 0.7
         gltf.scene.scale.set(.03,.03,.03) // 缩放模型
         scene.add(gltf.scene);
-        animate();
     })
 
     var animate = function() {
         orbit.update();
-        renderer.render(scene, camera);
+        // renderer.render(scene, camera);
+        composer.render() 
         requestAnimationFrame(animate);
     }
 };
